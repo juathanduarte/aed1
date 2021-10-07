18cfa05d9c71830fabc5909a60651fddfbb4d004
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2021-10-06 22:07:22 -0300

    semana 7: exercicio 3 40%

--- semana7/exercicio3/arvoreAVL.c
+++ semana7/exercicio3/arvoreAVL.c
@@ -1,6 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+typedef struct Nodo{
+    int key;
+    struct Nodo *pRight, *pLeft;
+}Nodo;
+
+void *addNodo(int number, Nodo *pRaiz);
+void insert(Nodo *pRaiz, Nodo *pNodo);
+void listThree(Nodo *pRaiz);
+void cleanThree(Nodo *pRaiz);
 
 int main(){
-  
+    Nodo *pRaiz = NULL;
+    int nNodos;
+    printf("Digite a quantidade de nodos: ");
+    scanf("%d", &nNodos);
+    pRaiz = addNodo(nNodos, pRaiz);
+    printf("\n");
+    listThree(pRaiz);
+    printf("\n");
+    cleanThree(pRaiz);
+
+}
+
+void *addNodo(int number, Nodo *pRaiz){ // Add Ok
+    int i;
+
+    srand(time(0));
+
+    for(i = 0;i<number;i++){ 
+        Nodo *pNodo = (Nodo *)malloc(sizeof(Nodo));
+
+        if(!pNodo){
+            printf("Erro de alocação de memória!");
+            exit(0);
+            }
+
+        pNodo->key = (rand());
+        pNodo->pRight = NULL;
+        pNodo->pLeft = NULL;
+
+    if(i == 0){
+        pRaiz = pNodo;
+
+    }
+    else{
+        insert(pRaiz, pNodo);
+
+    }
+
+}
+    return pRaiz;
+}
+
+void insert(Nodo *pRaiz, Nodo *pNodo){ //Inserir OK
+    Nodo *pInsert;
+    pInsert = pRaiz;
+ 
+    while(pInsert != NULL){
+
+        if(pInsert->key >= pNodo->key){
+            if(pInsert->pLeft == NULL){
+                pInsert->pLeft = pNodo;
+
+                return ;
+            }
+                pInsert = pInsert->pLeft;
+        }
+
+        else if(pInsert->key < pNodo->key){
+            if(pInsert->pRight == NULL){
+                pInsert->pRight = pNodo;
+                return;
+            }
+            pInsert = pInsert->pRight;
+        }
+    }
+}
+
+void listThree(Nodo *pRaiz) { //Listar ok
+    Nodo *pRun = pRaiz;
+
+    if(pRun != NULL) {
+       printf("%d(", pRun->key);
+        listThree(pRun->pLeft);
+        listThree(pRun->pRight);
+        printf(")");
+    }
+
+}
+
+void cleanThree(Nodo *pRaiz){
+    if(pRaiz == NULL){
+        return 0;
+    }
+    cleanThree(pRaiz->pLeft);
+    cleanThree(pRaiz->pRight);
+    free(pRaiz);
 }
