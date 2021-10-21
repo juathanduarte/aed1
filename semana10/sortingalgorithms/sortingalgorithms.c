da36c94d95859df46f2f4b410edf73a70e989312
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2021-10-21 14:43:17 -0300

    semana 10: README & Vetor aleatório

--- semana10/sortingalgorithms/sortingalgorithms.c
+++ semana10/sortingalgorithms/sortingalgorithms.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main (){
-  int sizeVetor = 0, random, i;
+  int sizeVetor = 0, random, option, i;
 
   int *pVetor = NULL;
 
@@ -18,9 +18,45 @@ int main (){
   for (i = 0; i < sizeVetor; i++){
     random = rand() % 100;
     pVetor[i] = random;
-
-    printf("%d\n", pVetor[i]);
   }
 
+  do {
+    printf("\Lista de Exercicios 5 - Juathan | Algoritmos de ordenacao.\n\n");
+    printf("\t1) Insertion Sort \n");
+    printf("\t2) Selection Sort: \n");
+    printf("\t3) Quick Sort \n");
+    printf("\t4) Merge Sort \n");
+    printf("\t4) Sair \n");
+    printf("\t- Opcao: ");
+    scanf("%d", &option);
+    getchar();
+
+    switch (option) {
+      case 1:
+        system("clear || cls");
+        // INSERTION SORT
+        break;
+      case 2:
+        system("clear || cls");
+        // SELECTION SORT
+        break;
+      case 3:
+        system("clear || cls");
+        // QUICK SORT
+        break;
+      case 4:
+        system("clear || cls");
+        // MERGE SORT
+        break;
+      case 5:
+        system("clear || cls");
+        // SAIR
+        break;
+      default:
+          system("clear || cls");
+          printf("\nOpcao invalida, digite novamente!\n");
+          break;
+        }
+    } while ((option >= 1) || (option <= 5));
 
 }
