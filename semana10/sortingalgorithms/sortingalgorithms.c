6761bb54e7ab6e0c0aade6685248c92359cdb618
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2021-10-25 15:43:08 -0300

    semana 10: insertion short && order vetor ok

--- semana10/sortingalgorithms/sortingalgorithms.c
+++ semana10/sortingalgorithms/sortingalgorithms.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+void orderVetor(int *pVetor, int sizeVetor);
+void insertionSort(int *pVetor, int sizeVetor);
+
 int main (){
   int sizeVetor = 0, random, option, i;
 
@@ -18,10 +21,11 @@ int main (){
   for (i = 0; i < sizeVetor; i++){
     random = rand() % 100;
     pVetor[i] = random;
+    printf("%d ", pVetor[i]);
   }
 
   do {
-    printf("\Lista de Exercicios 5 - Juathan | Algoritmos de ordenacao.\n\n");
+    printf("\n\nLista de Exercicios 5 - Juathan | Algoritmos de ordenacao.\n\n");
     printf("\t1) Insertion Sort \n");
     printf("\t2) Selection Sort: \n");
     printf("\t3) Quick Sort \n");
@@ -34,7 +38,18 @@ int main (){
     switch (option) {
       case 1:
         system("clear || cls");
-        // INSERTION SORT
+        printf("\n\n-- INSERTION SHORT --\n\n");
+        printf("- Vetor antes do Insertion Short: ");
+        for (i = 0; i < sizeVetor; i++){
+          printf("%d ", pVetor[i]);
+        }
+        orderVetor(pVetor, sizeVetor);
+        insertionSort(pVetor, sizeVetor);
+        printf("\n- Vetor depois do Insertion Short: ");
+        for (i = 0; i < sizeVetor; i++){
+          printf("%d ", pVetor[i]);
+        }
+        orderVetor(pVetor, sizeVetor);
         break;
       case 2:
         system("clear || cls");
@@ -59,4 +74,30 @@ int main (){
         }
     } while ((option >= 1) || (option <= 5));
 
+}
+
+void insertionSort(int *pVetor, int sizeVetor){
+  int auxNumber, auxCounter, counter;
+
+  for (counter = 1; counter < sizeVetor; counter++){
+    auxCounter = counter - 1;
+    auxNumber = pVetor[counter];
+    while((auxCounter >= 0) && (auxNumber < pVetor[auxCounter])){
+      pVetor[auxCounter + 1] = pVetor[auxCounter];
+      auxCounter--;
+    }
+    pVetor[auxCounter + 1] = auxNumber;
+  }
+}
+
+void orderVetor(int *pVetor, int sizeVetor){
+  int counter;
+
+  for (counter = 0; counter < (sizeVetor - 1); counter++){
+    if (pVetor[counter] > pVetor[counter + 1]){
+      printf("\nNão esta ordenado!\n");
+      return;
+    }
+  }
+  printf("\nEsta ordenado!");
 }
