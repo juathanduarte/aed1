19a40b86cd07385ed32e7ca31b2934c79b494e59
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2021-10-25 18:12:24 -0300

    semana 10: time algorithm ok

--- semana10/sortingalgorithms/sortingalgorithms.c
+++ semana10/sortingalgorithms/sortingalgorithms.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 
 void orderVetor(int *pVetor, int sizeVetor);
 void insertionSort(int *pVetor, int sizeVetor);
@@ -9,6 +9,8 @@ int main (){
   int sizeVetor = 0, random, option, i;
 
   int *pVetor = NULL;
+
+  struct timeval begin, end;
 
   printf ("Digite o tamanho do vetor: ");
   scanf("%d", &sizeVetor);
@@ -19,18 +21,18 @@ int main (){
   srand(time(NULL));
 
   for (i = 0; i < sizeVetor; i++){
-    random = rand() % 100;
+    random = rand() % 100000000;
     pVetor[i] = random;
     printf("%d ", pVetor[i]);
   }
 
   do {
     printf("\n\nLista de Exercicios 5 - Juathan | Algoritmos de ordenacao.\n\n");
-    printf("\t1) Insertion Sort \n");
-    printf("\t2) Selection Sort: \n");
-    printf("\t3) Quick Sort \n");
-    printf("\t4) Merge Sort \n");
-    printf("\t4) Sair \n");
+    printf("\t1) Insertion Sort\n");
+    printf("\t2) Selection Sort\n");
+    printf("\t3) Quick Sort\n");
+    printf("\t4) Merge Sort\n");
+    printf("\t4) Sair\n");
     printf("\t- Opcao: ");
     scanf("%d", &option);
     getchar();
@@ -39,17 +41,28 @@ int main (){
       case 1:
         system("clear || cls");
         printf("\n\n-- INSERTION SHORT --\n\n");
+
         printf("- Vetor antes do Insertion Short: ");
         for (i = 0; i < sizeVetor; i++){
           printf("%d ", pVetor[i]);
         }
         orderVetor(pVetor, sizeVetor);
+
+        gettimeofday(&begin, 0);
         insertionSort(pVetor, sizeVetor);
+        gettimeofday(&end, 0);
+        long seconds = end.tv_sec - begin.tv_sec;
+        long microseconds = end.tv_usec - begin.tv_usec;
+        double elapsed = seconds + microseconds*1e-6;
+
         printf("\n- Vetor depois do Insertion Short: ");
         for (i = 0; i < sizeVetor; i++){
           printf("%d ", pVetor[i]);
         }
         orderVetor(pVetor, sizeVetor);
+
+        printf("\nTempo para ordenar com o Insertion Short: %.3f segundos.\n", elapsed);
+
         break;
       case 2:
         system("clear || cls");
